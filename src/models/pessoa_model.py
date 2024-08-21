3f8ff99b6000b276b05b7b3259cc9d064955b51e
NinjaBitroom <gabrielbitercourt@gmail.com>
2024-08-21 10:50:04 -0400

    Simplifique os modelos da tabela cliente

--- src/models/pessoa_model.py
+++ src/models/pessoa_model.py
@@ -6,7 +6,7 @@ from src.services.database import db
 
 class PessoaModel(EnderecoModel):
     __abstract__ = True
-    nome: Mapped[str] = mapped_column(db.String(80), nullable=False)
-    cpf: Mapped[str] = mapped_column(db.String(11), nullable=False)
-    email: Mapped[str] = mapped_column(db.String(80), nullable=False)
-    telefone: Mapped[str] = mapped_column(db.String(11), nullable=False)
+    nome: Mapped[str] = mapped_column(db.String(80))
+    cpf: Mapped[str] = mapped_column(db.String(11))
+    email: Mapped[str] = mapped_column(db.String(80))
+    telefone: Mapped[str] = mapped_column(db.String(11))
